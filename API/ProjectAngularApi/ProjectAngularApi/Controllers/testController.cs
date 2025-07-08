cef1d77160b91285d142bb13822293507ae25bd5
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-09 01:46:34 +0300

    test

---
+++ API/ProjectAngularApi/ProjectAngularApi/Controllers/testController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAngularApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public string getwelcome() 
+        {
+
+            return "Welcome";
+        }
+            
+    }
+}
