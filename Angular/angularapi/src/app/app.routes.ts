95e3dd2e54f5b14537afce03da6c567dfd584b08
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-08 16:10:57 +0300

    Angular Project Structure

--- Angular/angularapi/src/app/app.routes.ts
+++ Angular/angularapi/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { NotFound } from './layout/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'exams', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'exams', loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) },
+  { path: 'results', loadChildren: () => import('./results/student-results').then(m => m.ResultsModule) },
+  { path: '**', component: NotFound }
+];
+
---
+++ Angular/angularapi/src/app/exams/take-exam/take-exam.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-take-exam',
+  imports: [],
+  templateUrl: './take-exam.html',
+  styleUrl: './take-exam.css'
+})
+export class TakeExam {
+
+}
