028625513dd4b6c6dd462a717aaed39263f66774
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-13 14:17:42 +0300

    Update Program and appSettings - Test Controller

--- API/ProjectAngularApi/ProjectAngularApi/Controllers/ExamController.cs
+++ API/ProjectAngularApi/ProjectAngularApi/Controllers/ExamController.cs
@@ -19,7 +19,7 @@ namespace ProjectAngularApi.Controllers
         {
             this.examRepo = examRepo;
         }
-        [Authorize]
+        [Authorize(Roles = "Admin", AuthenticationSchemes = "Bearer")]
         [HttpGet]
 
         public IActionResult GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
