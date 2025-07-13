af3f73b6bfca40e546a1c1b2d089222fa9ce0b5b
dawood74 <omardaved75@gmail.com>
2025-07-13 12:26:45 +0300

    modify angulur strucature

--- Angular/angularapi/src/app/app.routes.ts
+++ Angular/angularapi/src/app/app.routes.ts
@@ -1,21 +1,11 @@
+
 import { Routes } from '@angular/router';
-import { Login } from './modules/auth/login/login';
-import { Register } from './modules/auth/register/register';
-import { NotFound } from './modules/shared/components/not-found/not-found';
+import { NotFound } from '../app/Components/shared/not-found/not-found';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'exams', pathMatch: 'full' },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+{ path: '', redirectTo: 'Home', pathMatch: 'full' },
+  
 
-  // { path: 'exams', loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) },
-  // { path: 'results', loadChildren: () => import('./results/student-results').then(m => m.ResultsModule) },
-
-  // { path: 'exams', loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) },
-  // { path: 'results', loadChildren: () => import('./results/student-results').then(m => m.ResultsModule) },
-
-  //{ path: 'exams', loadChildren: () => import('./exams/exams.module').then(m => m.ExamsModule) },
-  //{ path: 'results', loadChildren: () => import('./results/student-results').then(m => m.ResultsModule) },
 
   { path: '**', component: NotFound }
 ];
--- Angular/angularapi/src/app/app.ts
+++ Angular/angularapi/src/app/app.ts
@@ -1,15 +1,19 @@
+
 import { Component } from '@angular/core';
-import { Header } from "./modules/shared/components/header/header";
-import { RouterLink, RouterModule } from '@angular/router';
-import { NotFound } from "./modules/shared/components/not-found/not-found";
+import { RouterModule } from '@angular/router';
+import { NotFound } from '../app/Components/shared/not-found/not-found';
+import { Header } from '../app/Components/shared/header/header';
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [RouterModule, NotFound, Header], 
+  imports: [RouterModule, NotFound, Header, NotFound], 
 })
 export class App {
   protected title = 'angularapi';
 }
+
+
