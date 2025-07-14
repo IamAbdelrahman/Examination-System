d34bb8a5d92fd7c9c3bff8932762d4ca3577a24c
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-07-14 18:55:46 +0300

    Auth Guards

--- Angular/angularapi/src/app/Components/auth/login/login.ts
+++ Angular/angularapi/src/app/Components/auth/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { TokenStorageService } from '../../../core/services/token-storage.service';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 @Component({
@@ -17,7 +17,7 @@ export class LoginComponent {
   password = '';
   error = '';
 
-  constructor(private http: HttpClient, private tokenStorage: TokenStorageService, private router: Router) {}
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService, private router: Router) { }
 
   login() {
     this.http.post<any>('https://localhost:44301/api/Auth/login', {
@@ -27,16 +27,20 @@ export class LoginComponent {
       next: (res) => {
         this.tokenStorage.saveToken(res.token);
         const decoded: any = jwtDecode(res.token);
-        const roles: string[] = decoded['roles'] || [];
-        if (roles.includes('Admin')) this.router.navigate(['/admin-dashboard']);
-        else this.router.navigate(['/admin-dashboard']);
+        const roles: string[] = JSON.parse(decoded.roles);
+
+        console.log(roles); // ✅ ["Admin"]
+        console.log(roles[0]); // ✅ "Admin"
+        console.log(decoded);
+        if (roles.includes('Admin')) this.router.navigate(['/exam']);
+        else this.router.navigate(['/exam-details']);
       },
       error: (err) => {
         this.error = 'Invalid email or password';
       }
     });
   }
-    navigateToRegister() {
+  navigateToRegister() {
     this.router.navigate(['/register']);
   }
 }
